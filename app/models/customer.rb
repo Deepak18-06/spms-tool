2a60a2e2631c37be5441b4906ced5ea768445de7
deepak <kumar.deepak.iiit@gmail.com>
2024-02-21 00:03:44 +0530

    changes

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,7 @@
 class Customer < ApplicationRecord
     has_many :quotations, dependent: :destroy
-    accepts_nested_attributes_for :quotations,
-                                allow_destroy: true
+    validates :first_name, :phone, :email, presence: true
+    validates :phone, length: { minimum: 10, maximum: 14 }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
+    accepts_nested_attributes_for :quotations, allow_destroy: true
 end
--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,4 +1,5 @@
 class Quotation < ApplicationRecord
   belongs_to :customer
+  validates :attachment, :date, presence: true
   has_one_attached :attachment
 end
